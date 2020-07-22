17578fec9b386749f46d44663dc425f210cdba14
67Samuel <60543441+67Samuel@users.noreply.github.com>
2020-07-22 20:16:23 +0800

    Update nn_helpers.py

--- nn_helpers.py
+++ nn_helpers.py
@@ -87,12 +87,18 @@ def train(args, optimizer, train_loader, val_loader, criterion=nn.CrossEntropyLo
     wandb.log({'percentage snipped':((1-hparams['snip_factor'])*100)})
     
     model = createAlexNet().to(device) # model for 200 classes
-    pytorch_alexnet = tv.models.alexnet(pretrained=True).to(device) #pretrained model for 1000 classes
+    if args.pretrain:
+        pytorch_alexnet = tv.models.alexnet(pretrained=True).to(device) #pretrained model for 1000 classes
     # apply SNIP
-    keep_masks = SNIP(pytorch_alexnet, hparams['snip_factor'], train_loader, device, img_size=args.img_size)
-    apply_prune_mask(pytorch_alexnet, keep_masks)
+    if args.pretrain:
+        keep_masks = SNIP(pytorch_alexnet, hparams['snip_factor'], train_loader, device, img_size=args.img_size)
+        apply_prune_mask(pytorch_alexnet, keep_masks)
+    else:
+        keep_masks = SNIP(model, hparams['snip_factor'], train_loader, device, img_size=args.img_size)
+        apply_prune_mask(model, keep_masks)
     # for transfer learning and shifting snipped weights over to model
-    copyLayerWeightsExceptLast(pytorch_alexnet, model, requires_grad=(not args.tl))
+    if args.pretrain:
+        copyLayerWeightsExceptLast(pytorch_alexnet, model, requires_grad=(not args.tl))
     model.to(device)
     # calculating percentage snipped
     net = tv.models.alexnet(pretrained=True).to(device)
