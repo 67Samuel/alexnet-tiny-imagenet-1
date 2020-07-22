53648f4b7e62abd9315fcf80d03e2fe94fb333ef
67Samuel <60543441+67Samuel@users.noreply.github.com>
2020-07-22 20:18:04 +0800

    Update nn_helpers.py

--- nn_helpers.py
+++ nn_helpers.py
@@ -110,7 +110,7 @@ def train(args, optimizer, train_loader, val_loader, criterion=nn.CrossEntropyLo
 
     opt = optimizer(model.parameters(), lr=hparams["init_lr"], weight_decay=hparams['weight_decay_rate'])
     lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-                   opt, patience=1, factor=0.5)
+                   opt, patience=args.lr_patience, factor=0.5)
     train_cross_entropy = []
     train_accuracy = []
     validation_cross_entropy = []
